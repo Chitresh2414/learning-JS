ece54c9765ec5614ce99eb49c1e18d65bc35025e
Chitresh2414 <Chitresharya123@gmail.com>
2025-09-01 17:47:32 +0530

    Add HTML and JavaScript files for learning examples; include inline, internal, and external scripts

---
+++ js/var-let-const.js
@@ -0,0 +1,15 @@
+var fname="chinku";
+var fname="monu";// redeclaring variable
+fname="chitresh";// reassigning variable
+console.log(fname);
+
+let lname="kumar";
+// let lname="singh"; redeclaring not allowed
+lname="singh";// reassigning variable
+console.log(lname);
+
+
+const pi=3.14;
+// const pi=3.14159; redeclaring not allowed
+// pi=3.14159; // reassigning not allowed
+console.log(pi);
---
+++ js/variable.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+
+const titleElement = document.querySelector("#title");
+titleElement.textContent = "Hello Chinku🥰";
